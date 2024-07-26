bd88fe2a869920b02112768418f62526622e935e
AMAN KUMAR <126344755+AMANKUMAR187@users.noreply.github.com>
2024-07-26 21:04:19 +0530

    Update hero.jsx

--- src/component/hero/hero.jsx
+++ src/component/hero/hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
         
         <div className={styles.content}> 
              <h1>Hi, I Am AMAN KUMAR</h1>
-            <p className={styles.discription}>Lorem10 vnvsdlvwd vsdnvsovnslvsdvlv sdlvspgs. sdlf vwlrksv lrkrgmgsdls.gnwlfwfsdsfpwpfs,mngpodf.sdpsdgnglsdflsdmwfweptwnwgmwr I'm a full-stack developer with 2 year of experience with React,Nodejs,Flutter reach out to learn more!</p>
+            <p className={styles.discription}>I am currently a second-year student pursuing a Bachelor of Technology in Computer Science And Technology at Birla Institute of Technology, Mesra. With a CGPA of 8.52, I am passionate about exploring the realms of data structures and algorithms (DSA) and full stack development. My journey in technology has led me to complete various projects and internships in Web and Android development, providing me with hands-on experience and expertise.</p>
            
             <a className={styles.contactbtn} >Downlode Resume</a>
         </div>
