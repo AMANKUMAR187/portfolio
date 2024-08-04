456651ea0669dbb3e8eb375fae5c3eba403681ae
AMAN KUMAR <126344755+AMANKUMAR187@users.noreply.github.com>
2024-08-04 17:56:48 +0530

    Update navbar.jsx

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -24,7 +24,7 @@ export const NavBar = () =>{
                 <li ><a href='#about'>About</a> </li>
                 <li ><a href='#projects'>Projects</a> </li>
                 <li ><a href='#contact'>Contact</a> </li>
-                <li ><a href='#experience'>experience</a> </li>
+                <li ><a href='#education'>Education</a> </li>
 
             </ul>
         </div>
