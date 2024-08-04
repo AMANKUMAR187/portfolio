082f6c1bef9fb01b1a67a2d7953cca1c8915370b
AMAN KUMAR <126344755+AMANKUMAR187@users.noreply.github.com>
2024-08-04 18:02:46 +0530

    cg improvement update

--- src/component/hero/hero.jsx
+++ src/component/hero/hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
         
         <div className={styles.content}> 
              <h1>Hi, I Am AMAN KUMAR</h1>
-            <p className={styles.discription}>I am currently a second-year student pursuing a Bachelor of Technology in Computer Science And Technology at Birla Institute of Technology, Mesra. With a CGPA of 8.52, I am passionate about exploring the realms of data structures and algorithms (DSA) and full stack development. My journey in technology has led me to complete various projects and internships in Web and Android development, providing me with hands-on experience and expertise.</p>
+            <p className={styles.discription}>I am currently a second-year student pursuing a Bachelor of Technology in Computer Science And Technology at Birla Institute of Technology, Mesra. With a CGPA of 8.53, I am passionate about exploring the realms of data structures and algorithms (DSA) and full stack development. My journey in technology has led me to complete various projects and internships in Web and Android development, providing me with hands-on experience and expertise.</p>
            
             <a className={styles.contactbtn} href='Aman1.pdf' download="Aman.pdf" > Downlode Resume</a>
         </div>
