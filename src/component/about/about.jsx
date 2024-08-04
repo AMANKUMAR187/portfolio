e6c39cb6b28fb1c69423e40d96c51d9b1706c16f
AMAN KUMAR <126344755+AMANKUMAR187@users.noreply.github.com>
2024-08-04 17:44:21 +0530

    Update about.jsx

--- src/component/about/about.jsx
+++ src/component/about/about.jsx
@@ -6,7 +6,7 @@ import { getImageUrl } from "../../utils";
 export const About = () => {
   return (
     <section className={styles.container} id="about">
-      <h2 className={styles.title}>About</h2>
+      <h2 className={styles.title}>About & Education</h2>
       <div className={styles.content}>
         <img
           src={getImageUrl("about/laptop.png")}
@@ -15,32 +15,42 @@ export const About = () => {
         />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <img src={getImageUrl("about/college.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
+              <h2>Birla Institute Of Technology Mesra, Ranchi</h2>
               <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
+                B.Tech in CSE : CGPA-8.52/10.00
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Server icon" />
             <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
+              <h2>Achievements</h2>
               <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
+              • 3rd PRIZE IN IEEE MEGA-PROJECT HACKATHON ,2023 among 140+ teams
+              </p>
+              <p>
+              • 1st PRIZE IN ESTIMANIA by Finance Club- Bit-Mesra, 2023 among 20+ teams
+              </p>
+              <p>
+              • Solved 500+ Questions On various Platform including leetcode,gfg,codechef,codeforces.
+              </p>
+              <p>
+              • 98.12%ile in Jee Main with 17k rank among 20lakh+ students 
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
             <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
+              <h2>Clubs & Society</h2>
               <p>
-                I have designed multiple landing pages and have created design
-                systems as well
+              • IEEE Student Branch-BIT Mesra
+              
+              </p>
+              <p>
+              • IET On Campus-BIT Mesra
               </p>
             </div>
           </li>
@@ -49,3 +59,57 @@ export const About = () => {
     </section>
   );
 };
+
+
+
+// import react from "react";
+
+// import styles from "./about.module.css";
+// import { getImageUrl } from "../../utils";
+
+// export const About = () => {
+//   return (
+//     <section className={styles.container} id="about">
+//       <h2 className={styles.title}>About</h2>
+//       <div className={styles.content}>
+//         <img
+//           src={getImageUrl("about/laptop.png")}
+//           alt="Me sitting with a laptop"
+//           className={styles.aboutImage}
+//         />
+//         <ul className={styles.aboutItems}>
+//           <li className={styles.aboutItem}>
+//             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+//             <div className={styles.aboutItemText}>
+//               <h3>Frontend Developer</h3>
+//               <p>
+//                 I'm a frontend developer with experience in building responsive
+//                 and optimized sites
+//               </p>
+//             </div>
+//           </li>
+//           <li className={styles.aboutItem}>
+//             <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+//             <div className={styles.aboutItemText}>
+//               <h3>Backend Developer</h3>
+//               <p>
+//                 I have experience developing fast and optimised back-end systems
+//                 and APIs
+//               </p>
+//             </div>
+//           </li>
+//           <li className={styles.aboutItem}>
+//             <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+//             <div className={styles.aboutItemText}>
+//               <h3>UI Designer</h3>
+//               <p>
+//                 I have designed multiple landing pages and have created design
+//                 systems as well
+//               </p>
+//             </div>
+//           </li>
+//         </ul>
+//       </div>
+//     </section>
+//   );
+// };
